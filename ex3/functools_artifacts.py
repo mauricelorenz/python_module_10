291158aa9482356264907fbe378dcfd1be80366f
Maurice Lorenz <me@mauricelorenz.com>
2026-03-03 23:35:28 +0100

    Finalize ex3

--- ex3/functools_artifacts.py
+++ ex3/functools_artifacts.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Callable
+from typing import Callable, Any
 from functools import reduce, partial, lru_cache, singledispatch
 from operator import add, mul
 
@@ -12,16 +12,43 @@ def spell_reducer(spells: list[int], operation: str) -> int:
     return reduce(operation_map[operation], spells)
 
 
+def base_enchantment(power: int, element: str, target: str) -> str:
+    return f"Hit {target} with {power} power {element}"
+
+
 def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
-    pass
+    return {"fire_enchant": partial(base_enchantment, 50, "Fire"),
+            "ice_enchant": partial(base_enchantment, 50, "Ice"),
+            "lightning_enchant": partial(base_enchantment, 50, "Lightning")}
 
 
+@lru_cache
 def memoized_fibonacci(n: int) -> int:
-    pass
+    if n < 2:
+        return n
+    return memoized_fibonacci(n - 1) + memoized_fibonacci(n - 2)
 
 
 def spell_dispatcher() -> Callable:
-    pass
+    @singledispatch
+    def dispatch_func(arg: Any) -> str:
+        return "Invalid argument type"
+
+    @dispatch_func.register(int)
+    def int_dispatch(arg: int) -> str:
+        return f"Making {arg} damage"
+
+    @dispatch_func.register(str)
+    def str_dispatch(arg: str) -> str:
+        return f"Enchanting item with {arg}"
+
+    @dispatch_func.register(list)
+    def list_dispatch(arg: list) -> str:
+        result_list = []
+        for spell in arg:
+            result_list.append(f"Cast spell: {spell}")
+        return ", ".join(result_list)
+    return dispatch_func
 
 
 def main() -> None:
@@ -32,7 +59,20 @@ def main() -> None:
     print(f"Product: {spell_reducer(spell_powers, operations[1])}")
     print(f"Max: {spell_reducer(spell_powers, operations[2])}")
     print(f"Min: {spell_reducer(spell_powers, operations[3])}")
+    print("\nTesting partial enchanter...")
+    enchantments = partial_enchanter(base_enchantment)
+    for enchantment in enchantments:
+        print(enchantments[enchantment]("Dragon"))
+    print("\nTesting memoized fibonacci...")
     fibonacci_tests = [16, 8, 15]
+    for nbr in fibonacci_tests:
+        print(f"Fib({nbr}): {memoized_fibonacci(nbr)}")
+    print("\nTesting spell dispatcher...")
+    spell = spell_dispatcher()
+    print(f"int: {spell(50)}")
+    print(f"str: {spell("Fire")}")
+    print(f"list: {spell(["Fire", "Water", "Air"])}")
+    print(f"dict: {spell({"fail": 1})}")
 
 
 if __name__ == "__main__":
